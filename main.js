2b0263c113e50ab30728f360a0a3283e32433a8b
Jeremy Reed <reeje76@gmail.com>
2021-04-15 17:17:19 -0400

    Refactor code to use sqlite lib w/ promises.

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
-const db = new sqlite3.Database('data/foo', (err) => { console.log('Opened database!')});
 
-const data = [];
+const test = async function (db) {
+  await db.exec('CREATE TABLE test (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR(64))');
 
-db.serialize(() => {
-  db.exec('CREATE TABLE test (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR(64))');
+  await db.exec('INSERT INTO test (name) VALUES ("Foo Bar")');
 
-  db.exec('INSERT INTO test (name) VALUES ("Foo Bar")');
+  let row = await db.get('SELECT * FROM test WHERE name="Foo Bar"');
 
-  db.get(
-    'SELECT * FROM test WHERE name="Foo Bar"',
-    (err, row) => {
-      if (err) {
-        console.log('err: ', err);
-      } else {
-        console.log('row: ', row);
-        data.push(row);
-      }
-    }
-  );
+  return row;
+}
+
+open({
+  filename: 'data/foo',
+  driver: sqlite3.Database
+}).then((db) => {
+  test(db)
+    .then((row) => {
+      console.log('Returned row: ', row);
+      db.close()
+        .then(() => {
+          console.log('Closed the database');
+        })
+        .catch((error) => {
+          console.log('Caught error while closing! ', error)
+        });
+    })
+    .catch(() => {
+      console.log('Caught error');
+    });
 });
