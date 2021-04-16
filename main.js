0777bcd72bb8435260757a9f519a1a6a8d521903
Jeremy Reed <reeje76@gmail.com>
2021-04-16 15:33:13 -0400

    Join where nameID matches given nameID.

--- main.js
+++ main.js
@@ -14,12 +14,21 @@ const testJoins = async function (db) {
   return joinResult;
 }
 
+const getAllRowsFromFooWithSameNameID = async function (db) {
+  let nameID = '3a12bc10-f90d-42b3-a32f-ed9d3500731fj';
+  let joinSameNameIDResult = await db.all('SELECT * FROM foo INNER JOIN test ON test.nameID = foo.nameID where foo.nameID = ?', nameID);
+
+  return joinSameNameIDResult;
+}
+
 const testDatabase = async function (db) {
   let allRows = await getAllRows(db);
   let joinResult = await testJoins(db);
+  let joinSameNameIDResult = await getAllRowsFromFooWithSameNameID(db);
 
   console.log('allRows: ', allRows);
   console.log('joinResult: ', joinResult);
+  console.log('joinSameNameIDResult: ', joinSameNameIDResult);
 }
 
 open({
