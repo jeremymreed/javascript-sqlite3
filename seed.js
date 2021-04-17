d83df59e8267458eff6d1f95b7a3d1c2773ff057
Jeremy Reed <reeje76@gmail.com>
2021-04-17 13:04:38 -0400

    Update seed script.
    Change table names, add new columns, change data values.

--- seed.js
+++ seed.js
@@ -1,29 +1,107 @@
 const Database = require('./Database');
 
 const seedDatabase = async function (db) {
-  let nameID0 = '3a12bc10-f90d-42b3-a32f-ed9d3500731fj'
-  let nameID1 = '6e487079-fa00-47e2-ba65-1da4ac7a1acc';
-  let nameID2 = '8bcb9c54-4966-4b7f-8ad8-6aa9340cf54c';
+  let templateId0 = '3a12bc10-f90d-42b3-a32f-ed9d3500731fj'
+  let templateId1 = 'eb877655-f5ed-449c-bd58-4db34dca2d17';
+
+  let instanceId0 = '8bcb9c54-4966-4b7f-8ad8-6aa9340cf54c';
+  let instanceId1 = '39879c5d-7eb2-4024-b686-6d4c3fbfbbe1';
 
   // Prices in cents.  Dinero.js will want its input this way.
   let price0 = 150;
-  let price1 = 225;
-  let price2 = 1000;
+  let price1 = 200;
 
-  await db.run('INSERT INTO test (nameID, name) VALUES (?, "Foo Bar")', nameID0);
-  await db.run('INSERT INTO test (nameID, name) VALUES (?, "Another name")', nameID1);
-  await db.run('INSERT INTO test (nameID, name) VALUES (?, "NULL")', nameID2);
+  let start = 1551398400000;
+  let end = 1551498400000;
+  let purchaseDate = 1551398400000;
 
-  await db.run('INSERT INTO foo (nameID, type, price) VALUES (?, "DAMN SQL BULLSHIT", ?)', nameID0, price0);
-  await db.run('INSERT INTO foo (nameID, type, price) VALUES (?, "Foo", ?)', nameID0, price0);
-  await db.run('INSERT INTO foo (nameID, type, price) VALUES (?, "Bar", ?)', nameID0, price0);
-  await db.run('INSERT INTO foo (nameID, type, price) VALUES (?, "Baz", ?)', nameID0, price0);
+  await db.run(' \
+    INSERT INTO item_templates \
+    ( \
+      templateId, \
+      make, \
+      model \
+    ) \
+    VALUES \
+    ( \
+      ?, \
+      "Crest", \
+      "Toothpaste" \
+    )',
+    templateId0
+  );
 
-  await db.run('INSERT INTO foo (nameID, type, price) VALUES (?, "Fart", ?)', nameID1, price1);
-  await db.run('INSERT INTO foo (nameID, type, price) VALUES (?, "Burp", ?)', nameID1, price1);
+  await db.run(' \
+    INSERT INTO item_templates \
+    ( \
+      templateId, \
+      make, \
+      model \
+    ) \
+    VALUES \
+    ( \
+      ?, \
+      "Ragu", \
+      "Vodka Sauce" \
+    )',
+    templateId1
+  );
 
-  await db.run('INSERT INTO foo (nameID, type, price) VALUES (?, "Ice Cube", ?)', nameID2, price2);
-  await db.run('INSERT INTO foo (nameID, type, price) VALUES (?, "Taylor Swift", ?)', nameID2, price2);
+  await db.run('INSERT INTO item_instances \
+    ( \
+      instanceId, \
+      templateId, \
+      source, \
+      price, \
+      start, \
+      end, \
+      purchaseDate \
+    ) \
+    VALUES \
+    ( \
+      ?, \
+      ?, \
+      "Target", \
+      ?, \
+      ?, \
+      ?, \
+      ? \
+    )',
+    instanceId0,
+    templateId0,
+    price0,
+    start,
+    end,
+    purchaseDate
+  );
+
+  await db.run('INSERT INTO item_instances \
+    ( \
+      instanceId, \
+      templateId, \
+      source, \
+      price, \
+      start, \
+      end, \
+      purchaseDate \
+    ) \
+    VALUES \
+    ( \
+      ?, \
+      ?, \
+      "Wegmans", \
+      ?, \
+      ?, \
+      ?, \
+      ? \
+    )',
+    instanceId1,
+    templateId0,
+    price1,
+    start,
+    end,
+    purchaseDate
+  );
 }
 
 Database.openDb().then((db) => {
